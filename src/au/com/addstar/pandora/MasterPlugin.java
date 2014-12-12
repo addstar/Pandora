a0f49b29b911805f6a6e31c0012c3e28a465a975
Schmoller <steven.schmoll@gmail.com>
2014-12-12 15:32:58 +1100

    Added 'ExtendedGP' module to provide protection of 1.8 items

--- src/au/com/addstar/pandora/MasterPlugin.java
+++ src/au/com/addstar/pandora/MasterPlugin.java
@@ -67,6 +67,7 @@ public class MasterPlugin extends JavaPlugin
 		registerModule("AngryPigmen", "au.com.addstar.pandora.modules.AngryPigmen");
 		registerModule("ClaimSelect", "au.com.addstar.pandora.modules.ClaimSelect", "GriefPrevention", "WorldEdit");
 		registerModule("AntiSpawnerChange", "au.com.addstar.pandora.modules.AntiSpawnerChange");
+		registerModule("ExtendedGP", "au.com.addstar.pandora.modules.GPExtendedProtection", "GriefPrevention");
 		
 		//TODO: Register additional modules here
 	}
