554ae4b168969210695095261d7bfd2f7a633d55
Schmoller <steven.schmoll@gmail.com>
2015-03-21 18:44:37 +1100

    Updated to Spigot 1.8.3 R0.1

--- src/au/com/addstar/pandora/modules/autosave/ChunkSaveTask.java
+++ src/au/com/addstar/pandora/modules/autosave/ChunkSaveTask.java
@@ -6,8 +6,8 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_8_R1.CraftChunk;
-import org.bukkit.craftbukkit.v1_8_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R2.CraftChunk;
+import org.bukkit.craftbukkit.v1_8_R2.CraftWorld;
 import org.bukkit.scheduler.BukkitTask;
 
 public class ChunkSaveTask implements Runnable
