f62ef2f7406fb89471d8c4a42cc8dfe7ef8ca119
Narimm <benjicharlton@gmail.com>
2019-01-11 16:21:08 +1000

    Move to reflection?

--- src/au/com/addstar/pandora/modules/ActionBlocker.java
+++ src/au/com/addstar/pandora/modules/ActionBlocker.java
@@ -9,12 +9,19 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Cancellable;
-import org.bukkit.event.EventHandler;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.entity.EntityInteractEvent;
+import org.bukkit.plugin.IllegalPluginAccessException;
+import org.bukkit.plugin.RegisteredListener;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,12 +37,28 @@ public class ActionBlocker extends AbstractModule implements Listener {
     private FileConfiguration mConfig;
     private HashMap<World, Integer[]> WorldLimits = new HashMap<>();
     private HashMap<World, List<String>> blockedEvents = new HashMap<>();
-    
-    
-    
+    private List<String> allEvents = new ArrayList<>();
+
     @Override
     public void onEnable() {
         loadConfig();
+        RegisteredListener registeredListener = new RegisteredListener(this, (listener, event) -> {
+            try {
+                listener.getClass().getDeclaredMethod("onAction", Event.class).invoke(listener, event);
+            }catch (NoSuchMethodException|IllegalAccessException|InvocationTargetException e){}
+        }, EventPriority.NORMAL, mPlugin, false);
+        EntityInteractEvent.getHandlerList().register(registeredListener);
+        for(String eventName:allEvents){
+            try {
+                Class clazz = Class.forName(eventName);
+                if(clazz.isAssignableFrom(EntityEvent.class)){
+                    HandlerList list = getHandlerList(clazz);
+                    list.register(registeredListener);
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
     
     @Override
@@ -81,6 +104,13 @@ public class ActionBlocker extends AbstractModule implements Listener {
                         int maxY = wsection.getInt("maxY", 255);
                         List<String> events = wsection.getStringList("actions");
                         blockedEvents.put(world,events);
+                        for(String e:events){
+                            boolean exists = false;
+                            for(String a:allEvents){
+                                if(a.equals(e))exists = true;
+                            }
+                            if(!exists)allEvents.add(e);
+                        }
                         WorldLimits.put(world, new Integer[]{minY, maxY});
                     } else {
                         log("WARNING: Unknown World \"" + wname + "\"!");
@@ -105,8 +135,9 @@ public class ActionBlocker extends AbstractModule implements Listener {
         }
         return true;
     }
-    @EventHandler(priority = EventPriority.HIGH,ignoreCancelled = true)
-    public void onAction(EntityEvent event){
+    public void onAction(Event e){
+        if(!(e instanceof EntityEvent))return;
+        EntityEvent event = (EntityEvent) e;
         if(!blockedEvents.containsKey(event.getEntity().getWorld()))return;
             World  world = event.getEntity().getWorld();
             debugLog(event.getClass().getName() + " is being checked for world "+ world.getName());
@@ -116,17 +147,32 @@ public class ActionBlocker extends AbstractModule implements Listener {
             }
             debugLog(event.getClass().getName() + " is being checked...");
             List<String> events =  blockedEvents.get(event.getEntity().getWorld());
-            if(events.contains(event.getClass().getName())){
-                if(event instanceof Cancellable){
+            if(events.contains(event.getClass().getName())) {
+                if (event instanceof Cancellable) {
                     ((Cancellable) event).setCancelled(true);
                     debugLog(event.getClass().getName() + " was cancelled by Action Blocker");
                     return;
-                }else{
+                } else {
                     debugLog("Event:" + event.getClass().getName() + " does not implement cancellable and could not be cancelled...");
                     return;
                 }
             }
-            debugLog(event.getClass().getName() + " was allowed to proceed");
+    }
+
+    private static HandlerList getHandlerList(Class<? extends Event> clazz) {
+        while(clazz.getSuperclass() != null && Event.class.isAssignableFrom(clazz.getSuperclass())) {
+            try {
+                Method method = clazz.getDeclaredMethod("getHandlerList");
+                method.setAccessible(true);
+                return (HandlerList)method.invoke(null);
+            } catch (NoSuchMethodException var2) {
+                clazz = clazz.getSuperclass().asSubclass(Event.class);
+            } catch (Exception var3) {
+                throw new IllegalPluginAccessException(var3.getMessage());
+            }
+        }
+
+        throw new IllegalPluginAccessException("Unable to find handler list for event " + clazz.getName());
     }
 }
 
